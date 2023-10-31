25486188ed20f86a552a70f76a6b35971a98600b
jordanashwood <jordanmace0124@gmail.com>
2023-10-31 09:16:58 -0400

    added HTML and styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </div>
+      </form>
       <h1>
         Charleston,<b>USA</b>
       </h1>
@@ -17,6 +31,13 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly Cloudy"
           />
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 75%</li>
+            <li>Wind: 13 km/h%</li>
+          </ul>
         </div>
       </div>
     </div>
