3f983a702895687639a8c8df5a9622b8b5d26331
jordanashwood <jordanmace0124@gmail.com>
2023-11-02 10:01:16 -0400

    Correct forecast from C to F

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6 d-inline-flex align-items-center">
           <WeatherIcon code={props.data.icon} size={52} />
-          <div className="float-left">
+          <div className="float-left m-2">
             <WeatherTemperature fahrenheit={props.data.temperature} />
           </div>
         </div>
